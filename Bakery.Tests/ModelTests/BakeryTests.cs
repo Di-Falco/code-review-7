07d15ba34b590cf06d73a2032597b5f06b12dbfd
Anthony DiFalco <aodifalco@gmail.com>
2022-07-15 09:11:58 -0700

    pass second test

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -13,5 +13,13 @@ namespace Bakery.Tests
 
       Assert.AreEqual(5, testBread.BuyBread(1));
     }
+
+    [TestMethod]
+    public void BuyBread_ReturnTheCostOfThreeLoaves_Double()
+    {
+      Bread testBread = new Bread(5, "", "");
+
+      Assert.AreEqual(10, testBread.BuyBread(3));
+    }
   }
 }
